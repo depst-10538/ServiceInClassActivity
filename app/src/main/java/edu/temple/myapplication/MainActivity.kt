1fb5e751d91fca624e30c628c2f763b5904cf180
depst-10538 <tur12015@temple.edu>
2025-11-11 14:58:19 -0500

    Implemented a timer service, viewable in the Log

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,8 +1,17 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import android.widget.Button
+
+lateinit var timerBinder : TimerService.TimerBinder
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,12 +19,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
+        val serviceConnection = object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                timerBinder = service as TimerService.TimerBinder
+                //isConnected = true
+            }
 
+            override fun onServiceDisconnected(name: ComponentName?) {
+                //isConnected = false
+            }
+        }
+
+        val timerIntent = Intent(this,TimerService::class.java)
+        bindService(timerIntent,serviceConnection, Context.BIND_AUTO_CREATE)
+
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+            if(!timerBinder.isRunning){
+                timerBinder.start(100)
+                findViewById<Button>(R.id.startButton).text = "PAUSE"
+            } else{
+                timerBinder.pause()
+                findViewById<Button>(R.id.startButton).text = "PAUSE"
+                if(timerBinder.paused) findViewById<Button>(R.id.startButton).text = "RESUME"
+            }
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            timerBinder.stop()
+            findViewById<Button>(R.id.startButton).text = "START"
         }
+    }
+
+    override fun onDestroy(){
+        //unbindService(serviceConnection)
+        super.onDestroy()
     }
 }
